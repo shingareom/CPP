2ad3665c3e4887cfa965007a8b6b0afbf052bdc5
shingareom <omshingare120@gmail.com>
2023-02-06 18:06:00 +0530

    add.cpp added, done.

---
+++ CODES/begining/add.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the number"<<endl;
+    cin>>a;cin>>b;
+    cout<<"the addition is "<<(a+b)<<endl;
+    return 0;
+}
