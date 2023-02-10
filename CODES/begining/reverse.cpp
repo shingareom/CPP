5903b2094a8148ecd5cc136481d2f2e829224c46
shingareom <omshingare120@gmail.com>
2023-02-10 17:05:00 +0530

    reverse.cpp added, done.

---
+++ CODES/begining/reverse.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << endl << "the name is : Samarth , ID is : 101 , mobile number : 8765544322 , designation : manager , address ";
+    return 0;
+}
