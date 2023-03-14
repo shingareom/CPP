85245dda121f5a2ba18c03be6fc7b1879b383294
shingareom <omshingare120@gmail.com>
2023-03-14 20:22:00 +0530

    life.cpp added, done.

---
+++ CODES/INHERITANCE/life.cpp
@@ -0,0 +1,27 @@
+int ability = 1;
+
+
+
+/*
+
+ability new_roal()
+{
+    int noSucess()
+    {
+        if (ability == 1)
+            return 1;
+        else
+            return 0;
+    }
+
+    {
+        while (!!noSucess())
+        {
+            tryagain();
+            if (Dead())
+                goto new_roal;
+        }
+    }
+}
+
+*/
