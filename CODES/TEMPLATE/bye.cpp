d2f92a7628aeb71d8cafb75f0c38916656dd4729
shingareom <omshingare120@gmail.com>
2023-03-30 21:54:00 +0530

    bye.cpp added, done.

---
+++ CODES/TEMPLATE/bye.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main()
+{
+    om1:
+    cout << "";
+    if(2 == 2)
+    {
+        om:
+        cout << " hi : ";
+        _sleep(200);
+    }
+    goto om1;
+    goto om1;
+    return 0;
+}
