2d4ad456614f69f9d297c9700bcbd15208a0c0fc
shingareom <omshingare120@gmail.com>
2023-04-01 19:26:00 +0530

    new.cpp added, done.

---
+++ CODES/TEMPLATE/new.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int ch;
+    while (1)
+    {
+        printf("Enter the number : ");
+        ch = getch();
+        printf("%d\n", ch);
+    }
+
+    return 0;
+}
