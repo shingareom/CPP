49cf1165d57eb5f2fe7cfa98a072f7ea4db7891b
shingareom <omshingare120@gmail.com>
2023-04-04 21:51:00 +0530

    temp_03.cpp added, done.

---
+++ CODES/TEMPLATE/temp_03.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class name
+{
+    
+};
+
+int main()
+{
+    int a = 9999;
+    cout << hex << a << endl;
+    cout << oct << a << endl;
+    cout << dec << a << endl;
+
+
+    has_facet
+    return 0;
+}
