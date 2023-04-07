ce591eb0dc81a534f88f88fdcf2b425ad207703f
shingareom <omshingare120@gmail.com>
2023-04-07 17:44:00 +0530

    pr01.cpp added, done.

---
+++ CODES/TulTech/pr01.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class base1
+{
+public:
+    int d1;
+    base1(int a)
+    {
+        d1 = a;
+        cout << "Const called !" << endl;
+    }
+};
+
+
+class base2
+{
+public:
+    int d2;
+    base2(int a)
+    {
+        cout << "Const called " << endl;
+    }
+};
+
+class derived : public base1, public base2
+{
+    int d3;
+
+public:
+    derived(int a, int b, int c) : base1(a), base2(b)
+    {
+        cout << "Const called 3" << endl;
+        d3 = c;
+    }
+    void printall()
+    {
+        cout << "The d1 is " << d1 << endl;
+        cout << "The d2 is " << d2 << endl;
+        cout << "The d3 is " << d3 << endl;
+    }
+};
+int main()
+{
+    derived d(1, 2, 3);
+    return 0;
+}
