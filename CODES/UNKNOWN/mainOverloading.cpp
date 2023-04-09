55ec6fb54c224cc65ab5abff7a9c1f317a09d36f
shingareom <omshingare120@gmail.com>
2023-04-09 19:25:00 +0530

    mainOverloading.cpp added, done.

---
+++ CODES/UNKNOWN/mainOverloading.cpp
@@ -0,0 +1,9 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(int u, char **ptr)
+// {
+//     cout << "u is " << u << endl;
+//     cout << "ptr is " << (ptr*) << endl;
+//     cout << "parameterized main";
+// }
